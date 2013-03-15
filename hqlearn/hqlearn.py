b4bf8f5bcf50d023e06137bdca3e9a32128a897d
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-03-15 08:53:44 -0400

    REF: Updated to new kabuki develop version.

--- hqlearn/hqlearn.py
+++ hqlearn/hqlearn.py
@@ -1,9 +1,10 @@
 from numpy import *
 import numpy as np
-
-import pymc as pm
-import kabuki
 import pandas as pd
+import pymc as pm
+
+import kabuki
+from kabuki.utils import stochastic_from_dist
 
 rand = random.random
 
@@ -55,6 +56,8 @@ def RL_likelihood(data, lrate, invtemp):
     '''Given data (first column is stim, second is action, third is reward)
     lrate and inverse temperature, produces the likelihood of the data given model and params'''
 
+    data = data.reset_index()
+
     # initialize problem
     V = .5*ones((2,2))
     # get number of trials
@@ -75,12 +78,9 @@ def RL_likelihood(data, lrate, invtemp):
     # this is actually a log likelihood
     return likelihood
 
-RL_like = pm.stochastic_from_dist(name="QLearn likelihood",
-                                  logp=RL_likelihood,
-                                  random=RL_generate,
-                                  dtype=np.dtype('O'), #pymc hack
-                                  mv=False)
-
+RL_like = stochastic_from_dist(name="QLearn likelihood",
+                               logp=RL_likelihood,
+                               random=RL_generate)
 
 class HRL(kabuki.Hierarchical):
     def create_knodes(self):
@@ -89,15 +89,15 @@ class HRL(kabuki.Hierarchical):
       # like _var, _subj or _bottom to the corresponding Knode object.
       # Of specific interest is the _bottom knode as that's what we'll
       # put into the likelihood
-      invtemp = self.create_family_exp('invtemp', value=1)
+      invtemp = self.create_family_gamma('invtemp', value=1)
       # value contains the starting value for the group parameter
       lrate = self.create_family_invlogit('lrate', value=0.5) # will automatically convert .5 to logit
 
       # likelihood
       like = kabuki.Knode(RL_like, 'RL_like', lrate=lrate['lrate_bottom'],
-				       invtemp=invtemp['invtemp_bottom'],
-				       col_name=['state', 'action', 'reward'],
-				       observed=True)
+                          invtemp=invtemp['invtemp_bottom'],
+                          col_name=['state', 'action', 'reward'],
+                          observed=True)
       # return all knodes as a list
       return invtemp.values() + lrate.values() + [like]
 
